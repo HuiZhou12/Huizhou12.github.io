c5d1d89e28dc90041fdf10c5222d4372b6bac3cf
zhouhui <2158232954@qq.com>
2025-10-08 18:40:21 +0800

    增加静态文件

---
+++ assets/project_ai-report.md.CZ4EFBYz.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as r}from"./chunks/framework.BBlOp56t.js";const l=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"project/ai-report.md","filePath":"project/ai-report.md","lastUpdated":null}'),a={name:"project/ai-report.md"};function o(p,c,s,n,i,d){return r(),t("div")}const m=e(a,[["render",o]]);export{l as __pageData,m as default};
